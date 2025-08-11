b9d6b776c71a8390fb2c287196baa32e68fec260
a-bugaev <a.bugaev.97@gmail.com>
2025-08-11 17:57:56 +0700

    hw_40: models registered in admin.py; django-extensions installed

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+"""
+register your models here
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import (
+    Order,
+    Master,
+    Service,
+    Review,
+)
+
+admin.site.register(Order)
+admin.site.register(Master)
+admin.site.register(Service)
+admin.site.register(Review)
