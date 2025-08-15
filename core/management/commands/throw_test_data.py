1e8e5035c70e8c08ba640435dac855dac1198cd8
a-bugaev <a.bugaev.97@gmail.com>
2025-08-15 16:00:48 +0700

    hw_40: script for parsing testing data to db completed; timezone adjusted;

--- core/management/commands/throw_test_data.py
+++ core/management/commands/throw_test_data.py
@@ -4,10 +4,11 @@ core/test_data.py
 Populates DB with example data for development, testing and demonstration purposes
 """
 
+import os
 import json
 import pathlib
 import random
-from datetime import datetime
+import datetime
 
 import requests
 import tqdm
@@ -15,7 +16,8 @@ import tqdm
 from django.core.management.base import BaseCommand
 from django.core.files.images import ImageFile
 from django.conf import settings
-from core.models import (
+from django.utils import timezone
+from core.models import (  # type: ignore  # pylint: disable=import-error
     Order,
     Master,
     Service,
@@ -29,10 +31,16 @@ class Command(BaseCommand):
     Populates DB with example data for development, testing and demonstration purposes
     """
 
-    BASE_DIR = settings.BASE_DIR
-    MEDIA_ROOT = settings.MEDIA_ROOT
-
     help = "Populates DB with example data for development, testing and demonstration purposes"
+
+    def __init__(self):
+        super().__init__()
+        self.google_drive_url_ = {}
+        self.pics_ids_ = {}
+        self.masters_ = {}
+        self.services_ = {}
+        self.orders_ = {}
+        self.reviews_ = {}
 
     def load_data(self):
         """
@@ -43,90 +51,107 @@ class Command(BaseCommand):
         ) as f:
             return json.loads(f.read())
 
-    def download_pics(self, google_drive_url, pics_ids):
+    def download_pics(self):
         """
         Downloads images from googledrive to media/ dir
         """
-        print("download pics...")
-        total = sum([len(dir_) for dir_ in pics_ids.values()])
+        print("downloading pics...")
+        total = sum([len(dir_) for dir_ in self.pics_ids_.values()])
         pbar = tqdm.tqdm(total=total)
-        for dir_name, content_dict in pics_ids.items():
-
+        for dir_name, content_dict in self.pics_ids_.items():
             for file_name, file_id in content_dict.items():
-                filepath = pathlib.Path(self.MEDIA_ROOT / dir_name / f"{file_name}.webp")
+                filepath = pathlib.Path(settings.MEDIA_ROOT / dir_name / f"{file_name}.webp")
                 filepath.parent.mkdir(parents=True, exist_ok=True)
                 if not filepath.is_file():
-                    response = requests.get(google_drive_url.format(file_id), timeout=5)
+                    response = requests.get(self.google_drive_url_.format(file_id), timeout=5)
                     if response.status_code == 200:
                         with open(filepath, "wb") as f:
                             f.write(response.content)
                 pbar.update(1)
         pbar.close()
-        print("done!\n")
 
-    def populate_db(self, masters, services, orders, reviews, decor_pics):
+    def remove_pics(self):
         """
-        manually adds data to db
+        django creates copy of each pic undef new unique name, so we remove originals
         """
+        for dir_name, content_dict in self.pics_ids_.items():
+            for file_name, _ in content_dict.items():
+                os.remove(settings.MEDIA_ROOT / dir_name / f"{file_name}.webp")
 
-        service_insts = {}
-        for i, service in enumerate(services, start=1):
-            inst = Service(
+    def _add_services(self):
+        for i, service in enumerate(self.services_, start=1):
+            rel_file_path = (settings.MEDIA_ROOT / "service" / f"{i}.webp").relative_to(
+                settings.BASE_DIR
+            )
+            Service(
+                id=i,
                 name=service["name"],
                 description=service["description"],
                 price=service["price"],
                 duration=random.randint(10, 300),
-                image=ImageFile(open(self.MEDIA_ROOT / "service" / f"{i}.webp", "rb")),
-            )
-            service_insts[i] = inst
-            inst.save()
+                image=ImageFile(open(rel_file_path, "rb"), name=f"{i}.webp"),
+            ).save()
 
-        master_insts = {}
-        for i, master in enumerate(masters, start=1):
+    def _add_masters(self):
+        for i, master in enumerate(self.masters_, start=1):
+            rel_file_path = (settings.MEDIA_ROOT / "master" / f"{i}.webp").relative_to(
+                settings.BASE_DIR
+            )
             inst = Master(
+                id=i,
                 name=master["name"],
                 bio=master["description"],
-                photo=ImageFile(open(self.MEDIA_ROOT / "master" / f"{i}.webp", "rb")),
+                photo=ImageFile(open(rel_file_path, "rb"), name=f"{i}.webp"),
                 experience=master["work_experience"],
-                services_provided=[service_insts[id_] for id_ in master["services"]],
             )
-            master_insts[i] = inst
             inst.save()
+            inst.services_provided.set([Service.objects.get(id=id_) for id_ in master["services"]])
 
-        order_insts = {}
-        for i, order in enumerate(orders, start=1):
+    def _add_orders(self):
+        for order in self.orders_:
             inst = Order(
                 client_name=order["client_name"],
-                phone="".join(random.choices(range(10), k=11)),
-                master=master_insts[order["master_id"]],
-                services=[service_insts[id_] for id_ in order["services"]],
-                appointment_date=datetime.combine(
-                    datetime.strptime(order["date"], "%Y-%m-%d").date(),
-                    datetime.time(random.randint(10, 20), 0),
+                phone="".join(random.choices([str(n) for n in range(10)], k=11)),
+                master=Master.objects.get(id=order["master_id"]),
+                appointment_date=timezone.make_aware(
+                    datetime.datetime.combine(
+                        datetime.datetime.strptime(order["date"], "%Y-%m-%d").date(),
+                        datetime.time(random.randint(10, 20), 0, 0),
+                    )
                 ),
             )
-            order_insts[i] = inst
             inst.save()
+            inst.services.set([Service.objects.get(id=id_) for id_ in order["services"]])
 
-        review_insts = {}
-        for i, review in enumerate(reviews, start=1):
+    def _add_reviews(self):
+        for review in self.reviews_:
             inst = Review(
                 text=review["text"],
                 client_name=review["client_name"],
-                master=master_insts[review["master_id"]],
-                services_were_provided=[service_insts[id_] for id_ in review["services"]],
+                master=Master.objects.get(id=review["master"]),
                 rating=review["rating"],
             )
-            review_insts[i] = inst
             inst.save()
-
-        decor_image_insts = {}
-        for name in decor_pics.keys():
-            inst = DecorImage(
-                image=ImageFile(open(self.MEDIA_ROOT / "decor" / f"{name}.webp", "rb"))
+            inst.services_were_provided.set(
+                [Service.objects.get(id=id_) for id_ in review["services"]]
             )
-            decor_image_insts[name] = inst
-            inst.save()
+
+    def _add_decor_images(self):
+        for name in self.pics_ids_["decor"].keys():
+            rel_file_path = (settings.MEDIA_ROOT / "decor" / f"{name}.webp").relative_to(
+                settings.BASE_DIR
+            )
+            DecorImage(image=ImageFile(open(rel_file_path, "rb"), name=f"{name}.webp")).save()
+
+    def populate_db(self):
+        """
+        manually adds data to db
+        """
+        self._add_services()
+        self._add_masters()
+        self._add_orders()
+        self._add_reviews()
+        self._add_decor_images()
 
     def clear_db(self):
         """
@@ -143,13 +168,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         (
-            google_drive_url_,
-            pics_ids_,
-            masters_,
-            services_,
-            orders_,
-            reviews_,
+            self.google_drive_url_,
+            self.pics_ids_,
+            self.masters_,
+            self.services_,
+            self.orders_,
+            self.reviews_,
         ) = self.load_data().values()
-
-        self.download_pics(google_drive_url_, pics_ids_)
-        self.populate_db(masters_, services_, orders_, reviews_, pics_ids_["decor"])
+        self.clear_db()
+        self.download_pics()
+        self.populate_db()
+        self.remove_pics()
